97dc4623e0edc53c1bb750b83b8df9d8a6204170
Chadleigh <80205867+Chadleigh@users.noreply.github.com>
2021-10-19 11:50:33 +0200

    Commit

---
+++ src/test/java/za/ac/cput/controller/PaymentControllerTest.java
@@ -0,0 +1,29 @@
+package za.ac.cput.controller;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentControllerTest{
+
+    @Test
+    void create(){
+
+    }
+
+    @Test
+    void read(){
+
+    }
+
+    @Test
+    void update(){
+
+    }
+
+    @Test
+    void delete(){
+
+    }
+
+}
+
