2713a502354eeb96b4f930d61a82e8b4604d7857
DamianDuToit <81615904+DamianDuToit@users.noreply.github.com>
2021-10-19 19:03:00 +0200

    Add files via upload

---
+++ src/test/java/za/ac/cput/controller/CustomerControllerTest.java
@@ -0,0 +1,23 @@
+package za.ac.cput.controller;
+//Author: Damian Du Toit (219200203)
+
+import org.junit.jupiter.api.Test;
+
+//Customer Controller Test Class
+class CustomerControllerTest {
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
