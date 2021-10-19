ff318ebbb88f7382b15442715cd70c303204e47a
Chadleigh <80205867+Chadleigh@users.noreply.github.com>
2021-10-19 11:49:13 +0200

    Commit

---
+++ src/main/java/za/ac/cput/service/PaymentService.java
@@ -0,0 +1,14 @@
+package za.ac.cput.service.;
+
+import org.junit.jupiter.api.Pay;
+import za.ac.cput.service.PaymentService;
+import za.ac.cput.service.PaymentService;
+
+import java.util.Set;
+
+public interface PaymentService extends IService <Payment, String> {
+    Set<Payment> getAll();
+}
+
+
+
