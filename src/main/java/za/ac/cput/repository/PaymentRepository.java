a9cfb898f7f331ca6ee24d381cd5a428f48a6aea
Chadleigh <80205867+Chadleigh@users.noreply.github.com>
2021-10-19 11:46:50 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/repository/PaymentRepository.java
@@ -0,0 +1,16 @@
+package za.ac.cput.repository;
+
+import za.ac.cput.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// Author: Chadleigh Dunbar (218065256)
+
+@Repository
+public
+
+
+
